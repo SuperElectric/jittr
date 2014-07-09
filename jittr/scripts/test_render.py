3b9a20198fcf4cfc8112e4fc3fabfdc2920509cb
Daniel Gould <daniel@daniel-Inspiron-1525.(none)>
2014-07-09 14:29:24 +0100

    test_render.py now renders image, copies to RAM, then converts to numpy array. For some reason the render is now completely black

--- jittr/scripts/test_render.py
+++ jittr/scripts/test_render.py
@@ -1,8 +1,11 @@
 from panda3d.core import loadPrcFileData
 from panda3d.core import WindowProperties
 from panda3d.core import AntialiasAttrib
+from panda3d.core import PNMImage
 from direct.showbase.ShowBase import ShowBase
 import argparse
+import numpy  # arrays and linear algebra
+from matplotlib import pyplot  # plotting
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -27,7 +30,7 @@ def parse_args():
 
     return result
 
-def render(obj_file_path, width, height):
+"""def render(obj_file_path, width, height):
     loadPrcFileData('', 'win-size ' + str(width) + ' ' + str(height) )
     #loadPrcFileData('', 'framebuffer-multisample 1 \n multisamples 2')
     base = ShowBase()
@@ -37,12 +40,42 @@ def render(obj_file_path, width, height):
     base.model.reparentTo(base.render)
     base.graphicsEngine.renderFrame()
     base.screenshot(namePrefix = 'render')
-    base.run()
+    base.run()"""
+
+def renderToArray(obj_file_path, width, height):    
+    loadPrcFileData('', 'win-size ' + str(width) + ' ' + str(height) )
+    base = ShowBase()
+    base.model = base.loader.loadModel(obj_file_path)
+    base.model.setPos(0,4000,0);
+    base.model.reparentTo(base.render)    
+    base.graphicsEngine.renderFrame()
+    image = PNMImage()
+    dr = base.camNode.getDisplayRegion(0)
+    dr.getScreenshot(image)
+    arrayImage = numpy.zeros((height, width, 3))
+    i = 0
+    j = 0
+    while (i<width):
+        while (j<height):
+            arrayImage[j,i,0] = image.getRed(i,j)
+            arrayImage[j,i,0] = image.getGreen(i,j)
+            arrayImage[j,i,0] = image.getBlue(i,j)
+            j = j+1
+        i = i+1
+    return arrayImage
 
 def main():
 
     args = parse_args()
-    render(args.input, args.width, args.height)
+    image = renderToArray(args.input, args.width, args.height)  # get array of pixels
+
+    # Get current axes, hide the tick marks
+    axes = pyplot.gca()
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+
+    axes.imshow(image)  # "plot"s the pixel array in the axes.
+    pyplot.show()  # display plotting window 
 
 
 # Runs this code if this file is executed as a script.
