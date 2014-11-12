9b4c69f41db18f1497d22c9a7055cea8aaa1b503
Daniel Gould <dg441@cam.ac.uk>
2014-11-12 13:56:09 +0000

    added lego folder

---
+++ jittr/lego/make_array.py
@@ -0,0 +1,23 @@
+import matplotlib.image as mpimg
+import numpy
+import os.path
+
+n=0
+
+output = numpy.memmap('images', dtype='uint8', mode='w+', shape=(111456, 9216)) 
+x = numpy.zeros((96,96), dtype='uint8')
+for i in range(111456):
+    if (i<10):   
+        name = "./renders/a000%s.png" % i
+    elif (i<100):
+        name = "./renders/a00%s.png" % i
+    elif (i<1000):
+        name = "./renders/a0%s.png" % i
+    else:
+        name = "./renders/a%s.png" % i
+    if not os.path.exists(name):
+        print "File not found"
+    x[:] = 85*numpy.sum(mpimg.imread(name), axis=2)
+    output[i][:] = x.reshape((9216))
+    if i%1000 == 0:
+        print i
