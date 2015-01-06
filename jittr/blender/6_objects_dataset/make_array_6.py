174b24bd9fafc4d97acf195a5db1ed0af86f4e8e
Daniel Gould <dg441@cam.ac.uk>
2015-01-06 13:01:43 +0000

    added labels array and 6_object_dataset folder

--- jittr/blender/6_objects_dataset/make_array_6.py
+++ jittr/blender/6_objects_dataset/make_array_6.py
@@ -2,7 +2,6 @@ import matplotlib.image as mpimg
 import numpy
 import os.path
 
-n=0
 
 colour_output = numpy.memmap('colour_array', dtype='uint8', mode='w+', shape=(3888, 9216, 2))
 alpha_output = numpy.memmap('alpha_array', dtype='uint8', mode='w+', shape=(3888, 9216, 2))
@@ -10,14 +9,15 @@ x = numpy.zeros((4,96,96), dtype='uint8')
 colour = numpy.zeros((96,96), dtype='uint8')
 alpha = numpy.zeros((96,96), dtype='uint8')
 for i in range(3888):
-    if (i<10):   
-        name = "./renders/a000%s.png" % i
-    elif (i<100):
-        name = "./renders/a00%s.png" % i
-    elif (i<1000):
-        name = "./renders/a0%s.png" % i
+    j = i+1
+    if (j<10):   
+        name = "./a000%s.png" % j
+    elif (j<100):
+        name = "./a00%s.png" % j
+    elif (j<1000):
+        name = "./a0%s.png" % j
     else:
-        name = "./renders/a%s.png" % i
+        name = "./a%s.png" % j
     if not os.path.exists(name):
         print "File not found"
     x[:] = mpimg.imread(name).swapaxes(1,2).swapaxes(0,1)
@@ -27,3 +27,13 @@ for i in range(3888):
     alpha_output[i][:] = alpha.reshape((9216))
     if i%1000 == 0:
         print i
+
+     
+labels = numpy.memmap('labels', dtype='int32', mode='w+', shape=(3888,5))
+for i in range(3888):
+    light = (i/972)
+    model = (i/162) % 6
+    elev = (i/18) % 9
+    azim = i % 18
+    labels[i][:] = [0,model,elev,azim,light]
+
